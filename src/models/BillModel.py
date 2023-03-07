8cee6502a2f6a3821fdf0984358169e468861c4b
ecam-b <ec.angaritas@gmail.com>
2023-03-07 00:54:18 -0500

    Entidades listas, hay que establecer la condición por parte del usuario

--- src/models/BillModel.py
+++ src/models/BillModel.py
@@ -10,4 +10,20 @@ class BillModel(db.Model):
 
   id = db.Column(db.Integer, primary_key=True, nullable=False)
   date_bill = db.Column(db.Date)
-  user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+  user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+  value = db.Column(db.Numeric(9))
+  type = db.Column(db.Integer)
+  observation = db.Column(db.String(120))
+
+
+  def __init__(self, date_bill, user_id, value, type, observation):
+    self.date_bill = date_bill
+    self.user_id = user_id
+    self.value = value
+    self.type = type
+    self.observation = observation
+
+
+class BillSchema(ma.Schema):
+  class Meta:
+    fields = ("id", "date_bill", "user_id", "value", "type", "observation")
