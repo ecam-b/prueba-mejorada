0d3668099ec6115aeae2d4c6dff0656f5866ba1a
ecam-b <ec.angaritas@gmail.com>
2023-03-08 10:53:54 -0500

    Listas las primeras 3 rutas de bill con sus respectivas documentaciones

--- src/routes/Bill.py
+++ src/routes/Bill.py
@@ -142,3 +142,101 @@ def add_bill():
 
   except Exception as ex:
     return jsonify({"message": str(ex)})
+  
+
+@bill_bp.route("/<id>", methods=["PUT"])
+def update_bill(id):
+  """
+  Actualizar una Bill
+  Actualizar una nueva bill en la base de datos.
+  ---
+  tags:
+  - Bill
+  parameters:
+  - name: id
+    in: path
+    required: true
+    description: ID de la Bill.
+    type: integer
+  - name: bill
+    in: body
+    required: true
+    description: Datos para actualizar una bill.
+    schema:
+      type: object
+      properties:
+        date_bill:
+          type: date
+          description: Fecha de creación de la bill.
+        user_id: 
+          type: integer
+          description: Usuario que actualiza la bill.
+        value: 
+          type: integer
+          description: Valor de la bill.
+        type:
+          type: integer
+          description: Tipo de bill.
+        observation: 
+          type: string
+          description: Observación adicional sobre la bill.
+      example:
+        date_bill: "12-12-12"
+        user_id: 1
+        value: 10
+        type: 1
+        observation: "Todo correcto"
+  responses:
+    200:
+      description: OK
+      schema:
+        type: object
+        properties:
+          date_bill:
+            type: date
+            description: Fecha de creación de la bill.
+          user_id: 
+            type: integer
+            description: Usuario que actualiza la bill.
+          value: 
+            type: integer
+            description: Valor de la bill.
+          type:
+            type: integer
+            description: Tipo de bill.
+          observation: 
+            type: string
+            description: Observación adicional sobre la bill.
+        example:
+          date_bill: "12-12-12"
+          user_id: 1
+          value: 10
+          type: 1
+          observation: "Todo correcto"
+    400:
+      description: No se pueden mostrar la Bills.
+    500:
+      description: Error en el servidor.
+  """
+  try:
+    bill = BillModel.query.get(id)
+    if not bill:
+      return jsonify({"message": "Bill no encontrada."}), 400
+    
+    data = request.json
+    if data["date_bill"]:
+      bill.date_bill = data["date_bill"]
+    if data["user_id"]:
+      bill.user_id = data["user_id"]
+    if data["value"]:
+      bill.value = data["value"]
+    if data["type"]:
+      bill.type = data["type"]
+    if data["observation"]:
+      bill.observation = data["observation"]
+    
+    db.session.commit()
+    return for_him.jsonify(bill)
+
+  except Exception as ex:
+    return jsonify({"message": str(ex)})
